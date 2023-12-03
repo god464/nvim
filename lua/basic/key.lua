35a71f145c3aaf8e4ddb51777f371bc62c4f0d3e
god464 <god464@qq.com>
2023-12-03 14:41:46 +0800

    add keybinding

---
+++ lua/basic/key.lua
@@ -0,0 +1,24 @@
+local opts = { noremap = true, silent = true }
+local sopt = { silent = true }
+local map = vim.keymap.set
+vim.g.mapleader = " "
+map("n", "<C-h>", "<C-w>h", opts)
+map("n", "<C-j>", "<C-w>j", opts)
+map("n", "<C-k>", "<C-w>k", opts)
+map("n", "<C-l>", "<C-w>l", opts)
+map("n", "<C-Up>", ":resize +2<CR>", opts)
+map("n", "<C-Down>", ":resize -2<CR>", opts)
+map("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+map("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+map("n", "<S-l>", ":bnext<CR>", opts)
+map("n", "<S-h>", ":bprevious<CR>", opts)
+map("n", "<leader>e", ":Lex 30<cr>", opts)
+map("v", "<", "<gv", opts)
+map("v", ">", ">gv", opts)
+map("v", "<A-j>", ":m .+1<CR>==", opts)
+map("v", "<A-k>", ":m .-2<CR>==", opts)
+map("v", "p", '"_dP', opts)
+map("x", "J", ":move '>+1<CR>gv-gv", opts)
+map("x", "K", ":move '<-2<CR>gv-gv", opts)
+map("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+map("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
