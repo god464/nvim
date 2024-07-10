ae3f6e547a47cd94275610248972e3ab0e1c607a
god464 <god464@users.noreply.github.com>
2024-07-10 16:46:23 +0800

    formatting

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,35 +1,34 @@
-return{
-    {
-        "folke/tokyonight.nvim",
-        opts={
-            style="storm",
-        },
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "storm",
     },
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        opts = {
-            indent = {
-                char = "│",
-                tab_char = "│",
-            },
-            scope = { show_start = false, show_end = false },
-            exclude = {
-                filetypes = {
-                    "help",
-                    "alpha",
-                    "dashboard",
-                    "neo-tree",
-                    "Trouble",
-                    "trouble",
-                    "lazy",
-                    "mason",
-                    "notify",
-                    "toggleterm",
-                    "lazyterm",
-                },
-            },
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    opts = {
+      indent = {
+        char = "│",
+        tab_char = "│",
+      },
+      scope = { show_start = false, show_end = false },
+      exclude = {
+        filetypes = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
         },
-        main="ibl",
+      },
     },
+    main = "ibl",
+  },
 }
-
