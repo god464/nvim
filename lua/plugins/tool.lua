cf58035650a766b9c7981f59b86fa0074ac30c9b
god464 <god464@users.noreply.github.com>
2024-09-04 08:21:20 +0800

    add a linter for yaml
    update lazy-lock

--- lua/plugins/tool.lua
+++ lua/plugins/tool.lua
@@ -22,7 +22,8 @@ return {
     event = "LspAttach",
     config = function()
       local lint = require("lint")
-      lint.linters_by_ft = { c = { "clangtidy" }, cpp = { "clangtidy" }, rust = { "clippy" }, lua = { "selene" } }
+      lint.linters_by_ft =
+        { c = { "clangtidy" }, cpp = { "clangtidy" }, rust = { "clippy" }, lua = { "selene" }, yaml = { "yamllint" } }
     end,
   },
   {
