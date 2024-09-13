9e539049c7b60b150a1c734a55bb7c1c5a002c48
god464 <god464@users.noreply.github.com>
2024-09-13 17:04:13 +0800

    update lazy-lock
    adding support for neovide

---
+++ lua/basic/neovide.lua
@@ -0,0 +1,12 @@
+local g = vim.g
+
+if g.neovide then
+  g.neovide_transparency = 0.8
+  g.neovide_floating_shadow = true
+  g.neovide_floating_z_height = 10
+  g.neovide_light_angle_degrees = 45
+  g.neovide_light_radius = 5
+  g.neovide_floating_blur_amount_x = 2.0
+  g.neovide_floating_blur_amount_y = 2.0
+  g.neovide_hide_mouse_when_typing = true
+end
