1be65015482860f1ac28b743f3e8448cd8a24db5
god464 <god464@users.noreply.github.com>
2024-09-14 12:01:07 +0800

    refactor

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -34,7 +34,8 @@ return {
   },
   {
     "lukas-reineke/indent-blankline.nvim",
-    dependencies = { "HiPhish/rainbow-delimiters.nvim" },
+    dependencies = "HiPhish/rainbow-delimiters.nvim",
+    main = "ibl",
     opts = function()
       local highlight = {
         "RainbowDelimiterRed",
@@ -45,38 +46,27 @@ return {
         "RainbowDelimiterViolet",
         "RainbowDelimiterCyan",
       }
+      local ws_hightlight = { "CursorColumn", "Whitespace" }
+
+      local hooks = require("ibl.hooks")
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+      end)
+      vim.g.rainbow_delimiters = { highlight = highlight }
+      if vim.fn.expand("%:p") ~= "" then vim.cmd.edit({ bang = true }) end
+      hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+
       return {
-        scope = { show_exact_scope = true, highlight = highlight },
-        whitespace = { highlight = highlight },
+        scope = { highlight = highlight },
+        whitespace = { highlight = ws_hightlight },
         exclude = { filetypes = { "help", "NvimTree", "trouble", "Trouble", "lazy", "mason", "notify" } },
       }
-    end,
-    config = function(_, opts)
-      local rainbow_delimiters = require("rainbow-delimiters")
-      local ibl = require("ibl")
-      local hooks = require("ibl.hooks")
-      if vim.fn.expand("%:p") ~= "" then vim.cmd.edit({ bang = true }) end
-      ibl.setup(opts)
-      vim.g.rainbow_delimiters = {
-        strategy = {
-          [""] = rainbow_delimiters.strategy["global"],
-          vim = rainbow_delimiters.strategy["local"],
-        },
-        query = {
-          [""] = "rainbow-delimiters",
-          lua = "rainbow-blocks",
-        },
-        highlight = {
-          "RainbowDelimiterRed",
-          "RainbowDelimiterYellow",
-          "RainbowDelimiterBlue",
-          "RainbowDelimiterOrange",
-          "RainbowDelimiterGreen",
-          "RainbowDelimiterViolet",
-          "RainbowDelimiterCyan",
-        },
-      }
-      hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
     end,
   },
   {
