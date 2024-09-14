68870334e0998f76bf759dd18aa5f7ab61f3ddf5
god464 <god464@users.noreply.github.com>
2024-09-14 09:46:23 +0800

    update lazy-lock
    adding more neovide config

--- lua/basic/neovide.lua
+++ lua/basic/neovide.lua
@@ -9,4 +9,5 @@ if g.neovide then
   g.neovide_floating_blur_amount_x = 2.0
   g.neovide_floating_blur_amount_y = 2.0
   g.neovide_hide_mouse_when_typing = true
+  g.neovide_cursor_animate_in_insert_mode = true
 end
