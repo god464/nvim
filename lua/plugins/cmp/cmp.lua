856274bb6ab29a75ef7b21ea20b57a92d6438d6b
god464 <god464@users.noreply.github.com>
2024-10-10 10:23:19 +0800

    bump(lock) update lazy-lock
    fix(rust): fix loadding error
    
    feat(rust): add crate cmp and more rust lsp settings

--- lua/plugins/cmp/cmp.lua
+++ lua/plugins/cmp/cmp.lua
@@ -1,83 +1,84 @@
 return {
-    "iguanacucumber/magazine.nvim",
-    dependencies = {
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-nvim-lsp-signature-help",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "saadparwaiz1/cmp_luasnip",
-      "onsails/lspkind.nvim",
-      "lukas-reineke/cmp-under-comparator",
-    },
-    event = { "InsertEnter", "CmdlineEnter" },
-    opts = function()
-      vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-      local lspkind = require("lspkind")
-      cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({ name = "lazydev" }, { name = "path" }, { { name = "cmdline" } }),
-      })
-      cmp.setup.cmdline({ "/" }, { mapping = cmp.mapping.preset.cmdline(), sources = { { name = "buffer" } } })
-      return {
-        snippets = { expand = function(args) luasnip.lsp_expand(args.body) end },
-        mapping = {
-          ["<C-p>"] = cmp.mapping.select_prev_item(),
-          ["<C-n>"] = cmp.mapping.select_next_item(),
-          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.close(),
-          ["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if luasnip.expand_or_locally_jumpable() then
-              luasnip.jump(1)
-            else
-              fallback()
-            end
-          end, { "i", "s", "c" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s", "c" }),
+  "iguanacucumber/magazine.nvim",
+  dependencies = {
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-nvim-lsp-signature-help",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "saadparwaiz1/cmp_luasnip",
+    "onsails/lspkind.nvim",
+    "lukas-reineke/cmp-under-comparator",
+  },
+  event = { "InsertEnter", "CmdlineEnter" },
+  opts = function()
+    vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    local lspkind = require("lspkind")
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({ name = "lazydev" }, { name = "path" }, { { name = "cmdline" } }),
+    })
+    cmp.setup.cmdline({ "/" }, { mapping = cmp.mapping.preset.cmdline(), sources = { { name = "buffer" } } })
+    return {
+      snippets = { expand = function(args) luasnip.lsp_expand(args.body) end },
+      mapping = {
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.close(),
+        ["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if luasnip.expand_or_locally_jumpable() then
+            luasnip.jump(1)
+          else
+            fallback()
+          end
+        end, { "i", "s", "c" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s", "c" }),
+      },
+      sources = cmp.config.sources({
+        { name = "lazydev", group_index = 0 },
+        { name = "crates" },
+        { name = "luasnip" },
+        { name = "nvim_lsp_signature_help" },
+        { name = "nvim_lsp" },
+        { name = "path" },
+        { name = "buffer" },
+      }),
+      formatting = { format = lspkind.cmp_format({ mode = "symbol", show_labelDetails = true }) },
+      sorting = {
+        comparators = {
+          cmp.config.compare.offset,
+          cmp.config.compare.exact,
+          cmp.config.compare.score,
+          cmp.config.compare.recently_used,
+          require("cmp-under-comparator").under,
+          cmp.config.compare.kind,
+          cmp.config.compare.sort_text,
+          cmp.config.compare.length,
+          cmp.config.compare.order,
         },
-        sources = cmp.config.sources({
-          { name = "lazydev", group_index = 0 },
-          { name = "luasnip" },
-          { name = "nvim_lsp_signature_help" },
-          { name = "nvim_lsp" },
-          { name = "path" },
-          { name = "buffer" },
-        }),
-        formatting = { format = lspkind.cmp_format({ mode = "symbol", show_labelDetails = true }) },
-        sorting = {
-          comparators = {
-            cmp.config.compare.offset,
-            cmp.config.compare.exact,
-            cmp.config.compare.score,
-            cmp.config.compare.recently_used,
-            require("cmp-under-comparator").under,
-            cmp.config.compare.kind,
-            cmp.config.compare.sort_text,
-            cmp.config.compare.length,
-            cmp.config.compare.order,
-          },
-        },
-      }
-    end,
-    config = function(_, opts)
-      local cmp = require("cmp")
-      cmp.setup(opts)
+      },
+    }
+  end,
+  config = function(_, opts)
+    local cmp = require("cmp")
+    cmp.setup(opts)
 
-      vim.api.nvim_create_autocmd("BufRead", {
-        desc = "Setup cmp buffer crates source",
-        pattern = "Cargo.toml",
-        callback = function() cmp.setup.buffer({ sources = { { name = "crates" } } }) end,
-      })
-    end,
+    vim.api.nvim_create_autocmd("BufRead", {
+      desc = "Setup cmp buffer crates source",
+      pattern = "Cargo.toml",
+      callback = function() cmp.setup.buffer({ sources = { { name = "crates" } } }) end,
+    })
+  end,
 }
--- lua/plugins/lsp/rustacea_nvim.lua
+++ lua/plugins/lsp/rustacea_nvim.lua
@@ -1,6 +1,17 @@
 return {
   "mrcjkb/rustaceanvim",
   dependencies = { "neovim/nvim-lspconfig" },
-  ft = "rust",
-  opts = {},
+  ft = { "rust" },
+  opts = {
+    server = {
+      default_settings = {
+        ["rust-analyzer"] = {
+          cargo = { allFeatures = true, loadOutDirsFromCheck = true, buildScripts = { enable = true } },
+          checkOnSave = true,
+          procMarco = { enable = true },
+        },
+      },
+    },
+  },
+  config = function(opts) vim.g.rustaceanvim = opts end,
 }
