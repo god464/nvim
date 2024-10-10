600e8755c114e69f5977d2ecdb2d60314e5508d5
god464 <god464@users.noreply.github.com>
2024-10-10 21:02:52 +0800

    bump(lock): update lazy-lock
    fix(lint): fix bugs

--- lua/plugins/tools/nvim_lint.lua
+++ lua/plugins/tools/nvim_lint.lua
@@ -1,17 +1,23 @@
 return {
   "mfussenegger/nvim-lint",
   event = "LspAttach",
-  config = function()
+  opts = {
+    c = { "clangtidy" },
+    cpp = { "clangtidy" },
+    rust = { "clippy" },
+    lua = { "selene" },
+    yaml = { "yamllint" },
+    python = { "ruff", "mypy", "pylint" },
+    sh = { "shellCheck" },
+    nix = { "statix_check", "deadnix" },
+  },
+  config = function(_, opts)
     local lint = require("lint")
-    lint.linters_by_ft = {
-      c = { "clangtidy" },
-      cpp = { "clangtidy" },
-      rust = { "clippy" },
-      lua = { "selene" },
-      yaml = { "yamllint" },
-      python = { "ruff", "mypy", "pylint" },
-      sh = { "shellCheck" },
-      nix = { "statix_check", "deadnix" },
-    }
+    lint.linters_by_ft = opts
+
+    vim.api.nvim_create_autocmd(
+      { "BufReadPost", "BufWritePost", "InsertLeave" },
+      { desc = "Lint", callback = function() lint.try_lint() end }
+    )
   end,
 }
