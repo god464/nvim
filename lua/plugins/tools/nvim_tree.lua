cf19f0a64d56b3f86659ad4eeaceacc6365fd80c
cat <god464@users.noreply.github.com>
2024-11-01 11:37:32 +0800

    use nvim-tree

---
+++ lua/plugins/tools/nvim_tree.lua
@@ -0,0 +1,21 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  cmd = "NvimTreeToggle",
+  opts = {
+    renderer = {
+      highlight_git = "icon",
+      highlight_opened_files = "icon",
+      highlight_diagnostics = "icon",
+      highlight_modified = "icon",
+      highlight_hidden = "icon",
+      highlight_bookmarks = "icon",
+      highlight_clipboard = "icon",
+    },
+    actions = { open_file = { quit_on_open = true } },
+    update_focused_file = { enable = true, update_root = true },
+    view = { float = { open_win_config = { border = "rounded" } } },
+  },
+
+  keys = { { "<Leader>op", "<cmd>NvimTreeToggle<CR>", desc = "File Explorer" } },
+}
