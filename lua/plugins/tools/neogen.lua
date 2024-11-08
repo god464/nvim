98861087177ce8146b91cbeeedc6f1a457f34282
god464 <god464@users.noreply.github.com>
2024-11-08 10:05:16 +0800

    fix(neogen): switch to nvim

--- lua/plugins/tools/neogen.lua
+++ lua/plugins/tools/neogen.lua
@@ -1,5 +1,5 @@
 return {
   "danymat/neogen",
-  opts = { snippet_engine = "luasnip" },
+  opts = { snippet_engine = "nvim" },
   keys = { { "<leader>g", function() require("neogen").generate() end, desc = "Generate annotation" } },
 }
