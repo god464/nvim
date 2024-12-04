11bda494df790face8df3b9a57066f37a8833cfb
god464 <god464@users.noreply.github.com>
2024-12-04 17:00:41 +0800

    fix: fix show

--- lua/plugins/tools/oil.lua
+++ lua/plugins/tools/oil.lua
@@ -4,7 +4,7 @@ return {
   cmd = "Oil",
   keys = { { "<Leader>o-", "<cmd>Oil<cr>", desc = "Oil" } },
   opts = {
-    columns = { "icon", "permissions", "size", "mtype" },
+    columns = { "permissions", "size", "icon", "mtype" },
     win_options = { signcolumn = "yes" },
     skip_confirm_for_simple_edits = true,
     cleanup_delay_ms = false,
