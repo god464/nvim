d29ea0f1fda8734293e7ddf5911e57ab81c7a336
god464 <god464@users.noreply.github.com>
2024-12-05 17:04:00 +0800

    fix: fix outline icons

---
+++ lua/plugins/tools/aerial.lua
@@ -0,0 +1,13 @@
+return {
+  "stevearc/aerial.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+  enabled = false,
+  cmd = "AerialToggle",
+  keys = { { "<leader>ol", "<cmd>AerialToggle<cr>", desc = "Toggle Aerial" } },
+  opts = {
+    layout = {
+      default_direction = "prefer_left",
+      auto_jump = true,
+    },
+  },
+}
--- lua/plugins/tools/outline.lua
+++ lua/plugins/tools/outline.lua
@@ -1,6 +1,11 @@
 return {
   "hedyhli/outline.nvim",
-  cmd = { "Outline" },
+  dependencies = "onsails/lspkind.nvim",
+  cmd = "Outline",
   keys = { { "<leader>ol", "<Cmd>Outline<CR>", desc = "Outline" } },
-  opts = { preview_window = { live = true }, symbols = { icon_source = "lspkind" } },
+  opts = {
+    outline_window = { position = "left" },
+    preview_window = { live = true, border = "rounded" },
+    symbols = { icon_source = "lspkind" },
+  },
 }
