990a03e5850bcabded652b794a9ceea5e45928a0
god464 <god464@users.noreply.github.com>
2024-12-07 10:29:03 +0800

    fix: fix bugs

--- lua/plugins/tools/aerial.lua
+++ lua/plugins/tools/aerial.lua
@@ -4,10 +4,11 @@ return {
   cmd = "AerialToggle",
   keys = { { "<leader>ol", "<cmd>AerialToggle<cr>", desc = "Toggle Aerial" } },
   opts = {
-    layout = {
-      default_direction = "prefer_left",
-      auto_jump = true,
-    },
+    layout = { default_direction = "prefer_left" },
+    highlight_on_hover = true,
     filter_kind = false,
+    close_on_select = true,
+    nav = { autojump = true, preview = true },
+    lsp = { diagnostics_trigger_update = true },
   },
 }
