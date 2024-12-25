321903d2db5aaa0cb453efb1a6e027884103f00a
god464 <god464@users.noreply.github.com>
2024-12-25 11:23:02 +0800

    feat: reset blink

--- lua/plugins/cmp/blink.lua
+++ lua/plugins/cmp/blink.lua
@@ -15,11 +15,38 @@ return {
     appearance = { nerd_font_variant = "mono" },
     completion = {
       accept = { auto_brackets = { enabled = true } },
+      list = { selection = "auto_insert" },
       documentation = {
         auto_show = true,
         window = { border = "rounded", scrollbar = false },
         auto_show_delay_ms = 0,
         update_delay_ms = 0,
+      },
+      menu = {
+        draw = {
+          components = {
+            kind_icon = {
+              text = function(ctx)
+                local icon = ctx.kind_icon
+                if ctx.item.source_name == "LSP" then
+                  local color_item =
+                    require("nvim-highlight-colors").format(ctx.item.documentation, { kind = ctx.kind })
+                  if color_item and color_item.abbr then icon = color_item.abbr end
+                end
+                return icon .. ctx.icon_gap
+              end,
+              highlight = function(ctx)
+                local highlight = "BlinkCmpKind" .. ctx.kind
+                if ctx.item.source_name == "LSP" then
+                  local color_item =
+                    require("nvim-highlight-colors").format(ctx.item.documentation, { kind = ctx.kind })
+                  if color_item and color_item.abbr_hl_group then highlight = color_item.abbr_hl_group end
+                end
+                return highlight
+              end,
+            },
+          },
+        },
       },
     },
     signature = { enabled = true, window = { border = "rounded" } },
