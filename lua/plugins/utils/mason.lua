bd8f7a167650fa1ebcbb689fbd8ac9020dc04aa3
god464 <god464@users.noreply.github.com>
2025-01-03 23:42:46 +0800

    fix: fix mason build cmd

--- lua/plugins/utils/mason.lua
+++ lua/plugins/utils/mason.lua
@@ -1,7 +1,7 @@
 return {
   "williamboman/mason.nvim",
-  cmd = { "Mason", "MasonUpdate"},
-  build = "MasonUpdate",
+  cmd = "Mason",
+  build = ":MasonUpdate",
   opts = { ui = { border = "rounded" } },
   keys = { { "<Leader>M", "<Cmd>Mason<CR>", desc = "Mason" } },
 }
