130101eb106402d9d125d819a65ca8367e7c4ac3
god464 <god464@users.noreply.github.com>
2025-02-08 15:45:41 +0800

    bump: lazy-lock

--- lua/plugins/motion/guess_indent.lua
+++ lua/plugins/motion/guess_indent.lua
@@ -1,5 +1,9 @@
 return {
   "NMAC427/guess-indent.nvim",
   event = "FileType",
-  opts = {},
+  config = function()
+    require("guess-indent").setup({})
+
+    vim.api.nvim_create_autocmd({ "InsertLeave" }, { desc = "Guess Indent", command = "GuessIndent" })
+  end,
 }
