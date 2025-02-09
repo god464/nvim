08d346c0e9367dace611b247453c958fe567f11f
god464 <god464@users.noreply.github.com>
2025-02-09 12:03:22 +0800

    fix(illuminate): fix keymaps

--- lua/plugins/tools/illuminate.lua
+++ lua/plugins/tools/illuminate.lua
@@ -3,8 +3,8 @@ return {
   event = "VeryLazy",
   opts = { filetypes_denylist = { "xxd", "floggraph" } },
   keys = {
-    { "]]", function() require("illuminate").goto_next_reference() end, desc = "Next reference" },
-    { "[[", function() require("illuminate").goto_prev_reference() end, desc = "Prev reference" },
+    { "]r", function() require("illuminate").goto_next_reference() end, desc = "Next reference" },
+    { "[r", function() require("illuminate").goto_prev_reference() end, desc = "Prev reference" },
   },
   config = function(_, opts) require("illuminate").configure(opts) end,
 }
