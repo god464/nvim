b6a88f1f15780d4cc4a6d4974f57df5c0fc46a7d
god464 <god464@users.noreply.github.com>
2025-02-18 22:12:14 +0800

    feat(utils): add `leetcode.nvim`

---
+++ lua/plugins/utils/leetcode.lua
@@ -0,0 +1,6 @@
+return {
+  "kawre/leetcode.nvim",
+  dependencies = { "ibhagwan/fzf-lua", "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim" },
+  cmd = "Leet",
+  opts = { lang = "rust", cn = { enabled = false, translator = true, translate_problems = true } },
+}
