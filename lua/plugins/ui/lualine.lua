a0fd85d3b0afe9d6b892a8db9fdaff9848cff41c
god464 <god464@users.noreply.github.com>
2025-02-18 08:43:04 +0800

    fix: fix lualine extenstions

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -3,6 +3,6 @@ return {
   event = "VeryLazy",
   dependencies = "nvim-tree/nvim-web-devicons",
   opts = {
-    extensions = { "lazy", "symbols-outline", "trouble", "mason", "quickfix", "oil", "overseer", "fzf", "toggleterm" },
+    extensions = { "lazy", "aerial", "trouble", "mason", "quickfix", "oil", "overseer", "fzf", "toggleterm" },
   },
 }
