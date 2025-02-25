0addb8ce7546f6c947fa7795479d9f0030520cde
god464 <god464@users.noreply.github.com>
2025-02-25 15:58:14 +0800

    fix: fix

--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -90,6 +90,7 @@ return {
         sqls = { on_attach = function(client, bufnr) require("sqls").on_attach(client, bufnr) end },
         lemminx = {},
         jdtls = {},
+        gradle_ls = {},
       },
     }
   end,
