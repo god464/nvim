085cc8a45e6e87195114e17a26e24bc7f0872d81
god464 <god464@users.noreply.github.com>
2025-03-21 21:35:47 +0800

    feat: add everforest

---
+++ lua/plugins/theme/everforest.lua
@@ -0,0 +1,17 @@
+return {
+  "neanias/everforest-nvim",
+  event = "UIEnter",
+  opts = {
+    background = "hard",
+    transparent_background_level = 2,
+    italics = true,
+    ui_contrast = "high",
+    spell_foreground = true,
+    diagnostic_text_highlight = true,
+    diagnostic_line_highlight = true,
+  },
+  config = function(opts)
+    require("everforest").setup(opts)
+    vim.cmd([[colorscheme everforest]])
+  end,
+}
