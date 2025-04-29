bdde981e287768afbc456c786672cbed7b529064
god464 <god464@users.noreply.github.com>
2025-04-29 21:50:18 +0800

    feat: add exclude filetypes

--- lua/plugins/ui/color.lua
+++ lua/plugins/ui/color.lua
@@ -2,5 +2,9 @@
 return {
   "brenoprata10/nvim-highlight-colors",
   event = "VeryLazy",
-  opts = { render = "virtual", enable_tailwind = true },
+  opts = {
+    render = "virtual",
+    enable_tailwind = true,
+    exclude_filetypes = { "Trouble", "alpha", "help", "lazy", "mason", "notify", "toggleterm", "trouble" },
+  },
 }
--- lua/plugins/ui/indent_blankline.lua
+++ lua/plugins/ui/indent_blankline.lua
@@ -12,7 +12,7 @@ return {
     return {
       indent = { char = "│", tab_char = "│" },
       scope = { show_start = false, show_end = false },
-      exclude = { filetypes = { "help", "trouble", "Trouble", "lazy", "toggleterm", "notify", "alpha" } },
+      exclude = { filetypes = { "help", "trouble", "Trouble", "lazy", "toggleterm", "notify", "alpha", "mason" } },
     }
   end,
 }
