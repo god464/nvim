b18f13ea9bb884c6318f79a0ef4fe1ec7dbf93c2
god464 <god464@users.noreply.github.com>
2025-06-17 09:40:19 +0800

    fix(yazi): fix netrw support

--- lua/plugins/file/yazi.lua
+++ lua/plugins/file/yazi.lua
@@ -1,8 +1,8 @@
 return {
   "mikavilpas/yazi.nvim",
-  cmd = "Yazi",
+  event = "VeryLazy",
   dependencies = "folke/snacks.nvim",
   init = function() vim.g.loaded_netrwPlugin = 1 end,
   keys = { { "<leader>o-", mode = "n", "<cmd>Yazi<cr>", desc = "Open yazi at the current file" } },
-  opts = {},
+  opts = { open_for_directories = true },
 }
