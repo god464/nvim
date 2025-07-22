89b18d45f6713f821ae16e813d293410e47d2284
god464 <god464@users.noreply.github.com>
2025-07-22 09:24:44 +0800

    refactor(dap): drop firefox

--- lua/plugins/dap/dap.lua
+++ lua/plugins/dap/dap.lua
@@ -73,30 +73,6 @@ return {
       },
     }
 
-    dap.configurations.javascript = {
-      {
-        name = "Debug with Firefox",
-        type = "firefox",
-        request = "launch",
-        reAttach = true,
-        url = "http://localhost:3000",
-        webRoot = "${workspaceFolder}",
-        firefoxExecutable = "firefox",
-      },
-    }
-
-    dap.configurations.typescript = {
-      {
-        name = "Debug with Firefox",
-        type = "firefox",
-        request = "launch",
-        reAttach = true,
-        url = "http://localhost:3000",
-        webRoot = "${workspaceFolder}",
-        firefoxExecutable = "firefox",
-      },
-    }
-
     dap.configurations.java = {
       {
         type = "java",
