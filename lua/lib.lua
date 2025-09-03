9a530aa683bdd7052d76f0999a11a4a68cf6a969
god464 <god464@users.noreply.github.com>
2025-09-03 21:25:01 +0800

    fix: fix

--- lua/lib.lua
+++ lua/lib.lua
@@ -7,12 +7,8 @@ end
 
 ---@return string
 function M.get_vue_ts_plugin()
-  local local_path = vim.fn.resolve(
-    vim.fn.simplify(
-      vim.fn.fnamemodify(vim.fn.exepath("vue-language-server"), ":h")
-        .. "/../lib/language-tools/packages/typescript-plugin/"
-    )
-  )
+  local local_path = vim.fs.dirname(vim.fn.exepath("vue-language-server"))
+    .. "/../lib/language-tools/packages/language-server/node_modules/@vue/typescript-plugin"
 
   return vim.uv.fs_stat(local_path) and local_path
     or "/usr/lib/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin"
--- lua/plugins/theme/catppuccin.lua
+++ lua/plugins/theme/catppuccin.lua
@@ -4,22 +4,9 @@ return {
   name = "catppuccin.nvim",
   event = "UIEnter",
   opts = {
-    integrations = {
-      fidget = true,
-      grug_far = true,
-      mason = true,
-      neotest = true,
-      diffview = true,
-      noice = true,
-      notify = true,
-      nvim_surround = true,
-      overseer = true,
-      snacks = { enable = true },
-      lsp_trouble = true,
-      dadbod_ui = true,
-      which_key = true,
-      navic = { enabled = true },
-    },
+    transparent_background = true,
+    float = { transparent = true },
+    auto_integrations = true,
   },
   config = function(_, opts)
     require("catppuccin").setup(opts)
