535b43c9a100335056858d8d3a1c63d00cc1f090
god464 <god464@users.noreply.github.com>
2025-09-27 00:13:08 +0800

    fix: fix

--- lua/plugins/theme/catppuccin.lua
+++ lua/plugins/theme/catppuccin.lua
@@ -19,7 +19,7 @@ return {
       "akinsho/bufferline.nvim",
       opts = function(_, opts)
         if (vim.g.colors_name or ""):find("catppuccin") then
-          opts.highlights = require("catppuccin.groups.integrations.bufferline").get_theme()
+          opts.highlights = require("catppuccin.special.bufferline").get_theme()
         end
       end,
     },
