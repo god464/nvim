edc10ff8173446b3d342318ed43d90e195466bd3
god464 <god464@users.noreply.github.com>
2025-10-22 22:03:51 +0800

    feat(tools): add kulala

---
+++ lua/plugins/tools/kulala.lua
@@ -0,0 +1,15 @@
+---@type LazyPluginSpec
+return {
+  "mistweaverco/kulala.nvim",
+  ft = { "http", "rest" },
+  keys = {
+    { "<leader>Hs", desc = "Send request" },
+    { "<leader>Ha", desc = "Send all requests" },
+    { "<leader>Hb", desc = "Open scratchpad" },
+  },
+  opts = {
+    global_keymaps = false,
+    global_keymaps_prefix = "<leader>H",
+    kulala_keymaps_prefix = "",
+  },
+}
