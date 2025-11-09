1e9d50ffef66137c9ef3440adf8b9e9ac9ea50a6
god464 <god464@users.noreply.github.com>
2025-11-09 09:44:21 +0800

    fix: fix

--- lua/plugins/lsp/lazy_dev.lua
+++ lua/plugins/lsp/lazy_dev.lua
@@ -4,12 +4,5 @@ return {
   ft = "lua",
   ---@module 'lazydev'
   ---@type lazydev.Config
-  opts = {
-    library = {
-      { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-      { path = "lazy.nvim", words = { "LazyPluginSpec" } },
-      { path = "snacks.nvim", words = { "Snacks" } },
-      { "nvim-dap-ui" },
-    },
-  },
+  opts = { library = { { path = "${3rd}/luv/library", words = { "vim%.uv" } }, "lazy.nvim" } },
 }
