ab732732fdd90029e659456b45fccea6e9cfa4ba
god464 <god464@users.noreply.github.com>
2025-12-15 21:38:45 +0800

    refactor: change nvim-treesitter to main

--- lua/plugins/treesitter/tree_sitter.lua
+++ lua/plugins/treesitter/tree_sitter.lua
@@ -3,14 +3,22 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   dependencies = "LiadOz/nvim-dap-repl-highlights",
-  main = "nvim-treesitter.configs",
-  event = "UIEnter",
-  cmd = { "TSUpdateSync", "TSUpdate", "TSInstallFromGrammar", "TSInstall" },
-  opts = {
-    highlight = { enable = true, additional_vim_regex_highlighting = false },
-    incremental_selection = { enable = true },
-    indent = { enable = true },
-    textobjects = { enable = true },
-    auto_install = true,
-  },
+  config = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("treesitter.setup", {}),
+      callback = function(args)
+        local buf = args.buf
+        local filetype = args.match
+        local language = vim.treesitter.language.get_lang(filetype) or filetype
+        if not vim.treesitter.language.add(language) then return end
+
+        vim.wo.foldmethod = "expr"
+        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+        vim.treesitter.start(buf, language)
+
+        vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
 }
