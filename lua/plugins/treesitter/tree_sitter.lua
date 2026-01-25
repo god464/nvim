eb6c9715efe10c9d58d432cb088f7d740772b32c
god464 <god464@users.noreply.github.com>
2026-01-25 12:12:32 +0800

    fix: fix

--- lua/plugins/treesitter/tree_sitter.lua
+++ lua/plugins/treesitter/tree_sitter.lua
@@ -4,24 +4,31 @@ return {
   build = ":TSUpdate",
   event = "LazyFile",
   config = function()
-    local enable_ts = function(buf, lang)
-      if not vim.tbl_contains(require("nvim-treesitter.config").get_available(), lang) then return end
-      vim.wo.foldmethod = "expr"
-      vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-      vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-      vim.treesitter.start(buf, lang)
-    end
-
     vim.api.nvim_create_autocmd("FileType", {
       pattern = "*",
       group = vim.api.nvim_create_augroup("ts_setup", {}),
       callback = function(event)
-        local filetype = event.match
-        local lang = vim.treesitter.language.get_lang(filetype)
-        if not vim.tbl_contains(require("nvim-treesitter.config").get_installed("parsers"), lang) then
-          require("nvim-treesitter").install(lang):await(function() enable_ts(event.buf, lang) end)
+        local buf = event.buf
+        local ft = event.match
+        local lang = vim.treesitter.language.get_lang(ft)
+        if not lang then return end
+
+        local ts_config = require("nvim-treesitter.config")
+
+        if not vim.tbl_contains(ts_config.get_available(), lang) then return end
+
+        local function start_ts()
+          if not vim.treesitter.language.add(lang) then return end
+          vim.wo.foldmethod = "expr"
+          vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+          vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          vim.treesitter.start(buf, lang)
+        end
+
+        if not vim.tbl_contains(ts_config.get_installed("parsers"), lang) then
+          require("nvim-treesitter").install(lang):await(start_ts)
         else
-          enable_ts(event.buf, lang)
+          start_ts()
         end
       end,
     })
