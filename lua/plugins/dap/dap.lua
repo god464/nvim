b423c9c260e67d6b578dccb65380f44c101c1db2
god464 <god464@users.noreply.github.com>
2026-03-27 00:37:57 +0800

    fix: fix

--- lua/plugins/dap/dap.lua
+++ lua/plugins/dap/dap.lua
@@ -1,12 +1,7 @@
 ---@type LazyPluginSpec
 return {
   "mfussenegger/nvim-dap",
-  dependencies = {
-    "mfussenegger/nvim-dap-python",
-    "jbyuki/one-small-step-for-vimkind",
-    "leoluz/nvim-dap-go",
-    "nvim-lua/plenary.nvim",
-  },
+  dependencies = { "jbyuki/one-small-step-for-vimkind", "nvim-lua/plenary.nvim", "rcarriga/nvim-dap-ui" },
   keys = {
     {
       "<leader>dB",
@@ -32,10 +27,6 @@ return {
   config = function()
     local dap = require("dap")
 
-    local function prompt_path(prompt, default_path)
-      return function() return vim.fn.input(prompt, vim.fn.getcwd() .. default_path, "file") end
-    end
-
     vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
 
     local vscode = require("dap.ext.vscode")
@@ -43,84 +34,81 @@ return {
     vscode.json_decode = function(str) return vim.json.decode(json.json_strip_comments(str)) end
 
     require("dap-python").setup("python")
+    require("dap-go").setup()
 
-    for name, adapter in pairs({
-      codelldb = {
-        type = "server",
-        port = "${port}",
-        executable = { command = "codelldb", args = { "--port", "${port}" } },
+    dap.adapters.codelldb = {
+      type = "server",
+      port = "${port}",
+      executable = { command = "codelldb", args = { "--port", "${port}" } },
+    }
+
+    dap.adapters.haskell = { type = "executable", command = "haskell-debug-adapter" }
+
+    dap.configurations.cpp = {
+      {
+        name = "Launch file",
+        type = "codelldb",
+        request = "launch",
+        program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end,
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
       },
-      haskell = { type = "executable", command = "haskell-debug-adapter" },
-      coreclr = {
-        type = "executable",
-        command = "netcoredbg",
-        args = { "--interpreter=vscode" },
+    }
+
+    dap.configurations.c = dap.configurations.cpp
+
+    dap.adapters.coreclr = {
+      type = "executable",
+      command = "netcoredbg",
+      args = { "--interpreter=vscode" },
+    }
+
+    dap.configurations.lua = {
+      {
+        type = "nlua",
+        request = "attach",
+        name = "Attach to running Neovim instance",
       },
-    }) do
-      dap.adapters[name] = adapter
-    end
+    }
+
+    dap.configurations.java = {
+      {
+        type = "java",
+        request = "attach",
+        name = "Debug (Attach) - Remote",
+        hostName = "127.0.0.1",
+        port = 5005,
+      },
+    }
 
     dap.adapters.nlua = function(callback, config)
       callback({ type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 })
     end
 
-    local configurations = {
-      cpp = {
-        {
-          name = "Launch file",
-          type = "codelldb",
-          request = "launch",
-          program = prompt_path("Path to executable: ", "/"),
-          cwd = "${workspaceFolder}",
-          stopOnEntry = false,
-        },
-      },
-      lua = {
-        {
-          type = "nlua",
-          request = "attach",
-          name = "Attach to running Neovim instance",
-        },
-      },
-      java = {
-        {
-          type = "java",
-          request = "attach",
-          name = "Debug (Attach) - Remote",
-          hostName = "127.0.0.1",
-          port = 5005,
-        },
-      },
-      haskell = {
-        {
-          type = "haskell",
-          request = "launch",
-          name = "Debug",
-          workspace = "${workspaceFolder}",
-          startup = "${file}",
-          stopOnEntry = true,
-          logFile = vim.fn.stdpath("data") .. "/haskell-dap.log",
-          logLevel = "WARNING",
-          ghciEnv = vim.empty_dict(),
-          ghciPrompt = "λ: ",
-          ghciInitialPrompt = "λ: ",
-          ghciCmd = "stack ghci --test --no-load --no-build --main-is TARGET --ghci-options -fprint-evld-with-show",
-        },
-      },
-      cs = {
-        {
-          type = "coreclr",
-          name = "launch - netcoredbg",
-          request = "launch",
-          program = prompt_path("Path to dll", "/bin/Debug/"),
-        },
+    dap.configurations.haskell = {
+      {
+        type = "haskell",
+        request = "launch",
+        name = "Debug",
+        workspace = "${workspaceFolder}",
+        startup = "${file}",
+        stopOnEntry = true,
+        logFile = vim.fn.stdpath("data") .. "/haskell-dap.log",
+        logLevel = "WARNING",
+        ghciEnv = vim.empty_dict(),
+        ghciPrompt = "λ: ",
+        ghciInitialPrompt = "λ: ",
+        ghciCmd = "stack ghci --test --no-load --no-build --main-is TARGET --ghci-options -fprint-evld-with-show",
       },
     }
 
-    configurations.c = configurations.cpp
-
-    for filetype, config in pairs(configurations) do
-      dap.configurations[filetype] = config
-    end
+    dap.configurations.cs = {
+      {
+        type = "coreclr",
+        name = "launch - netcoredbg",
+        request = "launch",
+        program = function() return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file") end,
+      },
+    }
   end,
 }
