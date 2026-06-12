f0d8cd8831a4e9adf8a1dd38576754d26beefd19
god464 <god464@users.noreply.github.com>
2026-06-12 20:46:34 +0800

    fix: fix blink.pairs build

--- lua/plugins/motion/blink.lua
+++ lua/plugins/motion/blink.lua
@@ -1,8 +1,10 @@
 ---@type LazyPluginSpec
 return {
   "saghen/blink.pairs",
-  event = "VeryLazy",
   version = "*",
+  dependencies = "saghen/blink.lib",
+  event = "VeryLazy",
+  build = function() require("blink.pairs").download():pwait(60000) end,
   ---@module 'blink.pairs'
   ---@type blink.pairs.Config
   opts = {
