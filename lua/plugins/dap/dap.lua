740b9a81e995f791fb10b9d27faab9c539709637
god464 <god464@users.noreply.github.com>
2026-06-30 19:57:26 +0800

    fix rust

--- lua/plugins/dap/dap.lua
+++ lua/plugins/dap/dap.lua
@@ -54,7 +54,21 @@ return {
     }
 
     dap.configurations.c = dap.configurations.cpp
-    dap.configurations.rust = dap.configurations.cpp
+    dap.configurations.rust = {
+      {
+        initCommands = function()
+          local rustc_sysroot = vim.fn.trim(vim.fn.system("rustc --print sysroot"))
+          assert(vim.v.shell_error == 0, "failed to get rust sysroot using `rustc --print sysroot`: " .. rustc_sysroot)
+          local script_file = rustc_sysroot .. "/lib/rustlib/etc/lldb_lookup.py"
+          local commands_file = rustc_sysroot .. "/lib/rustlib/etc/lldb_commands"
+
+          return {
+            ([[!command script import '%s']]):format(script_file),
+            ([[command source '%s']]):format(commands_file),
+          }
+        end,
+      },
+    }
     dap.configurations.zig = dap.configurations.cpp
 
     dap.adapters.coreclr = {
